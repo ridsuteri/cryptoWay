c2c07086ee58062880e52ec468ed44664142cc95
Riddhi Suteri <ridsuteri@gmail.com>
2021-10-01 00:02:54 +0530

    add loader and fix mobile navigation issue

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,13 +6,15 @@ import { Link } from 'react-router-dom'
 import { useGetCryptosQuery } from '../services/cryptoApi'
 import { Cryptocurrencies } from '../components'
 import News from './News';
+import Loader from './Loader'
+
 const {Title} = Typography;
 
 const Homepage = () => {
     const { data, isFetching} = useGetCryptosQuery(10)
     const globalStats = data?.data?.stats;
     
-    if(isFetching) return <div>Loading...</div>
+    if(isFetching) return <Loader />
     return (
         <>
             <Title level={2} className="heading">
